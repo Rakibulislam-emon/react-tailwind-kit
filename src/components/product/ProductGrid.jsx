e497cd33ca9b0aac5cab7f8f61bcd0648ba40fb1
Rakibulislam-emon <rakibulislamemon60@gmail.com>
2025-05-22 13:41:50 +0600

    html tamplate converted into react and add dynamic product rendering using static data

--- src/components/product/ProductGrid.jsx
+++ src/components/product/ProductGrid.jsx
@@ -1,20 +1,12 @@
+import productsData from "../../data/products";
 import ProductCard from "./ProductCard";
 
 export default function ProductGrid() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
+      {productsData.map((product, idx) => (
+        <ProductCard key={idx} product={product} />
+      ))}
     </div>
   );
 }
--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,3 +1,7 @@
-export default function Button() {
-  return <button>Button</button>;
+export default function Button({ text }) {
+  return (
+    <button className="w-full mt-2 bg-red-800 py-1 text-gray-100 rounded flex items-center justify-center">
+       {text}
+    </button>
+  );
 }
